533fc44a8cb20515b0648bae57cf1493e8404fdb
dhbcoursera <dheidelberger@fpa.org>
2015-01-25 15:58:53 -0500

    Clarified and expanded several comments

--- cachematrix.R
+++ cachematrix.R
@@ -9,30 +9,39 @@
 
 
 ## Creates the matrix and inverse cache
-## Once you have initialized the CacheMatrix object,
-## you can call $set or $get to re-initialize it to a new
-## matrix. For example, if you have a matrix m and a 
+## Once you have initialized the CacheMatrix object (actually, a list),
+## you can call $get to retrieve it or $set to re-initialize 
+## it to a new matrix. For example, if you have a matrix m and a
 ## CacheMatrix cm, and you want to set cm to m, call
 ## cm$set(m). To view the contents, call cm$get()
 
 makeCacheMatrix <- function(x = matrix()) {
-    inverse <- NULL ##Initializes the inverse cache to null
+    inverse <- NULL # Initializes the inverse cache to null
     
     # Creates a function to set x to a new matrix
     set <- function(y) {
-        x <<- y
+        # x is saved in the makeCacheMatrix environment
+        # The <<- operator searhces through parent environments
+        # for a variable with that name. We have such a variable
+        # in the makeCacheMatrix environment so that is where
+        # it is assigned.
+        x <<- y 
+        
+        # inverse is reset to null. If we didn't do this, we would
+        # still have the old cached inverse even after
+        # assigning a new value to the original matrix.
         inverse <<- NULL
     }
-    get <- function() x #returns the current matrix
+    get <- function() x # Returns the current matrix
     
-    #This function saves the calculated inverse in the cache
+    # This function saves the calculated inverse in the cache
     setinverse <- function(solve) inverse <<- solve
     
-    #This function returns the inverse, null if it isn't cached yet
+    # This function returns the inverse, null if it isn't cached yet
     getinverse <- function() inverse
     
-    #List of all the available functions for the object
-    #so that they can be called with $ notation
+    # List containing all the functions for the "object."
+    # This is what gets returned.
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -42,31 +51,31 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## This function sees if the inverse has already been calculated and cached
 ## If it has, it returns the cached version, if it hasn't, it calculates
-## 
+## the inverse and returns it.
 
 cacheSolve <- function(x, ...) {
     # Return a matrix that is the inverse of 'x'
     
-    #If x does not have a cached inverse, this will be null
+    # If x does not have a cached inverse, this will be null
     inverse <- x$getinverse()
     
-    #Checks for a cached inverse. If there is one, it returns it
+    # Checks for a cached inverse. If there is one, it returns it
     if(!is.null(inverse)) {
         message("getting cached data")
         return(inverse)
     }
     
-    #If we get here, the inverse hasn't been calculated and there's no cache
+    # If we get here, the inverse hasn't been calculated and there's no cache
     
     data <- x$get()
     
-    #Here is where the inverse is actually calculated
+    # Here is where the inverse is actually calculated
     inverse <- solve(data, ...)
     
-    #Inverse cached back in the original object
+    # Inverse cached back in the original object
     x$setinverse(inverse)
     
-    #Return the inverse
+    # Return the inverse
     inverse
     
 }
